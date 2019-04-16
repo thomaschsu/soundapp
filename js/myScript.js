52c4fd71eff4756c0a4ce88c77d14affbc2118b7
Tom Hsu <thomaschsu@gmail.com>
2019-04-16 16:17:04 -0400

    Modified js

--- js/myScript.js
+++ js/myScript.js
@@ -1,7 +1,18 @@
+var circles = [];
+
 // On keydown generate new circle
 function onKeyDown(event) {
     var maxPoint = new Point(view.size.width, view.size.height);
     var randomPoint = Point.random();
     var point = maxPoint * randomPoint;
-    new Path.Circle(point, 10).fillColor = 'orange';
+    var newCircle = new Path.Circle(point, 500);
+    newCircle.fillColor = "orange"
+    circles.push(newCircle);
+}
+
+function onFrame(event){
+    for(var i = 0; i < circles.length; i++) {
+        circles[i].fillColor.hue += 1;
+        circles[i].scale(.9);
+    }
 }
